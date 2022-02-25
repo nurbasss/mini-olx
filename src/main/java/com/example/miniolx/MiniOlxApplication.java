2c2033176363dc6e025c1b9edf7a10f2f166b279
nurbasss <nurbakhyt.25@mail.ru>
2022-02-25 10:45:59 +0600

    initial

---
+++ src/main/java/com/example/miniolx/MiniOlxApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniolx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniOlxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniOlxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniolx/MiniOlxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniolx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniOlxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
